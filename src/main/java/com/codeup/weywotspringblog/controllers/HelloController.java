acb2b61dea8bef1c234bc70093cfd0f65c3a9df4
Javier Ruedas <jruedas@twc.com>
2022-11-15 14:02:09 -0600

    complete controllers lecture

--- src/main/java/com/codeup/weywotspringblog/controllers/HelloController.java
+++ src/main/java/com/codeup/weywotspringblog/controllers/HelloController.java
@@ -2,6 +2,7 @@ package com.codeup.weywotspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,4 +13,11 @@ public class HelloController {
     public String helloWorld() {
         return "Hello World";
     }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name){
+        return "Hello " + name + "!";
+    }
+
 }
