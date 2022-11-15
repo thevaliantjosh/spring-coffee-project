b7acf40c089d1cdf4e1cfbf05016c017f845dae3
Javier Ruedas <jruedas@twc.com>
2022-11-15 13:37:00 -0600

    initialize repo

---
+++ src/main/java/com/codeup/weywotspringblog/WeywotSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.weywotspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeywotSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeywotSpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/weywotspringblog/WeywotSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.weywotspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeywotSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
