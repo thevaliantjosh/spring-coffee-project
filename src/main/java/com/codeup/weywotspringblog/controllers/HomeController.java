725412e79424e72f9cb3d7e14fe3e91db7eb0e91
Javier Ruedas <jruedas@twc.com>
2022-11-16 14:56:50 -0600

    complete intro to thymeleaf

--- src/main/java/com/codeup/weywotspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/weywotspringblog/controllers/HomeController.java
@@ -2,14 +2,12 @@ package com.codeup.weywotspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String landingPage(){
-        return "This is the landing page!";
+        return "index";
     }
 }
